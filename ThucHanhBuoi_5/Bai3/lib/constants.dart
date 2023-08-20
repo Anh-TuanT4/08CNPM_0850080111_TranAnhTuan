9ffcb19a61914572c552edcf77bbcc39ca7197e4
[Bw] <trananhtuan20010@gmail.com>
2023-08-20 12:50:04 +0700

    Update

---
+++ ThucHanhBuoi_5/Bai3/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// ignore: constant_identifier_names
+const String google_api_key = "AIzaSyA7s4xlfvMmqC6msNgrnKb-YgSUopdyufQ";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
